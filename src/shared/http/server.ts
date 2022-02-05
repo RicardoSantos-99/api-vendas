a38a7831641b7d1205e64fc2ab7c340c4c0e38f0
RicardoSantos-99 <ricardo2010carvalho@hotmail.com.br>
2022-02-05 12:46:19 -0300

    feat: configs for run database

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,7 +2,8 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import routes from './routes';
-import AppError from './errors/AppError';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 app.use(cors());
