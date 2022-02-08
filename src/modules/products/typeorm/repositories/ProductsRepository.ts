8f884bd60d512b0c653daf2fcfa9713b0d6890bd
RicardoSantos-99 <ricardo2010carvalho@hotmail.com.br>
2022-02-08 08:34:22 -0300

    feat: create customRepository

---
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,9 @@
+import { EntityRepository, Repository } from 'typeorm';
+import Product from '@modules/products/typeorm/entities/Product';
+
+@EntityRepository(Product)
+export class ProductsRepository extends Repository<Product> {
+	public async findByName(name: string): Promise<Product | undefined> {
+		return this.findOne({ where: { name } });
+	}
+}
