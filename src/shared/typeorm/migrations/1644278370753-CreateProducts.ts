eeffbf6f8e62c4376a8611a085f0e08696d16ff4
RicardoSantos-99 <ricardo2010carvalho@hotmail.com.br>
2022-02-07 22:15:43 -0300

    feat: create product entity

--- src/shared/typeorm/migrations/1644278370753-CreateProducts.ts
+++ src/shared/typeorm/migrations/1644278370753-CreateProducts.ts
@@ -2,6 +2,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateProducts1644278370753 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
 		await queryRunner.createTable(
 			new Table({
 				name: 'products',
