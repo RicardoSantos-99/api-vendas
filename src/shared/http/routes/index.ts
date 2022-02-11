a31c5e3f465f1deb9062a6944a793da00350af5f
RicardoSantos-99 <ricardo2010carvalho@hotmail.com.br>
2022-02-10 23:59:18 -0300

    feat: product routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import productsRouter from '@modules/products/routes/product.routes';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-	return response.json({ message: 'Hello World' });
-});
+routes.use('/products', productsRouter);
 
 export default routes;
