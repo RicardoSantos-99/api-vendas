b6627bd78a3e920e6fafc496b7919fe1b403739b
RicardoSantos-99 <ricardo2010carvalho@hotmail.com.br>
2022-02-11 08:45:23 -0300

    feat: add validations products

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
@@ -10,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
 app.use(
 	(
