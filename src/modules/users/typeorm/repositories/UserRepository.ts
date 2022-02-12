2e3b59797984259c0c1f37f79c586f4ee1acd31a
RicardoSantos-99 <ricardo2010carvalho@hotmail.com.br>
2022-02-12 10:13:39 -0300

    feat: user repository

---
+++ src/modules/users/typeorm/repositories/UserRepository.ts
@@ -0,0 +1,19 @@
+import User from '../entities/User';
+import { EntityRepository, Repository } from 'typeorm';
+
+@EntityRepository(User)
+class UserRepository extends Repository<User> {
+	public async findByName(name: string): Promise<User | undefined> {
+		return await this.findOne({ where: { name } });
+	}
+
+	public async findById(id: string): Promise<User | undefined> {
+		return await this.findOne({ where: { id } });
+	}
+
+	public async findByEmail(email: string): Promise<User | undefined> {
+		return await this.findOne({ where: { email } });
+	}
+}
+
+export default UserRepository;
