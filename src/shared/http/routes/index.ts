bf581a96a7b980a1fb2524326aa0661135ed291f
RicardoSantos-99 <ricardo2010carvalho@hotmail.com.br>
2022-02-12 10:45:15 -0300

    feat(User): user routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import productsRouter from '@modules/products/routes/product.routes';
+import usersRouter from '@modules/users/routes/user.routes';
 
 const routes = Router();
 
+routes.use('/users', usersRouter);
 routes.use('/products', productsRouter);
 
 export default routes;
