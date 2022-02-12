ba033ddf8e6271cdb094f22c87825c999efff156
RicardoSantos-99 <ricardo2010carvalho@hotmail.com.br>
2022-02-12 11:53:27 -0300

    feat(User): add bcrypt and function for hashing password

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -18,10 +19,12 @@ class CreateUserService {
 		if (emailExists)
 			throw new AppError('There is already a user with this email');
 
+		const hashedPassword = await hash(password, 8);
+
 		const user = usersRepository.create({
 			name,
 			email,
-			password,
+			password: hashedPassword,
 		});
 
 		await usersRepository.save(user);
