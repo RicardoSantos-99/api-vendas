1b9aa0164a49dfede325cf2fb42826255250cc34
RicardoSantos-99 <ricardo2010carvalho@hotmail.com.br>
2022-02-12 10:19:58 -0300

    feat: createUserService

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,33 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UserRepository from '../typeorm/repositories/UserRepository';
+
+interface IRequest {
+	name: string;
+	password: string;
+	email: string;
+}
+
+class CreateUserService {
+	public async execute({ name, email, password }: IRequest): Promise<User> {
+		const userRepository = getCustomRepository(UserRepository);
+
+		const emailExists = await userRepository.findByName(email);
+
+		if (emailExists)
+			throw new AppError('There is already a user with this email');
+
+		const user = userRepository.create({
+			name,
+			email,
+			password,
+		});
+
+		await userRepository.save(user);
+
+		return user;
+	}
+}
+
+export default CreateUserService;
