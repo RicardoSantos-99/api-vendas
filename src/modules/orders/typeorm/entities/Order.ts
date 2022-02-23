11fb33d00d0a4ffc30afbea1d718b373afb3c7c6
RicardoSantos-99 <ricardo2010carvalho@hotmail.com.br>
2022-02-22 21:57:15 -0300

    feat(Orders): order-products entities

--- src/modules/orders/typeorm/entities/Order.ts
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -4,9 +4,11 @@ import {
 	Entity,
 	JoinColumn,
 	ManyToOne,
+	OneToMany,
 	PrimaryGeneratedColumn,
 	UpdateDateColumn,
 } from 'typeorm';
+import OrdersProducts from './OrdersProducts';
 
 @Entity('orders')
 class Order {
@@ -16,6 +18,11 @@ class Order {
 	@ManyToOne(() => Customer)
 	@JoinColumn({ name: 'customer_id' })
 	customer: Customer;
+
+	@OneToMany(() => OrdersProducts, order_products => order_products.order, {
+		cascade: true,
+	})
+	order_products: OrdersProducts[];
 
 	@CreateDateColumn()
 	created_at: Date;
