234978f7baca3639e51a52d2b18450179d5226cd
RicardoSantos-99 <ricardo2010carvalho@hotmail.com>
2022-02-24 07:44:00 -0300

    refactor(Product): change for export default

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,6 +1,6 @@
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
-import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+import ProductRepository from '../typeorm/repositories/ProductsRepository';
 
 class ListProductService {
 	public async execute(): Promise<Product[]> {
--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -2,8 +2,10 @@ import { EntityRepository, Repository } from 'typeorm';
 import Product from '@modules/products/typeorm/entities/Product';
 
 @EntityRepository(Product)
-export class ProductRepository extends Repository<Product> {
+class ProductRepository extends Repository<Product> {
 	public async findByName(name: string): Promise<Product | undefined> {
 		return this.findOne({ where: { name } });
 	}
 }
+
+export default ProductRepository;
