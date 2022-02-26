c77cfafbc695982712abe6124ab46af425d3f162
RicardoSantos-99 <ricardo2010carvalho@hotmail.com>
2022-02-26 08:27:06 -0300

    feat(Global): redis and ioredis

---
+++ src/config/cache.ts
@@ -0,0 +1,19 @@
+import { RedisOptions } from 'ioredis';
+
+interface ICacheConfig {
+	config: {
+		redis: RedisOptions;
+	};
+	driver: string;
+}
+
+export default {
+	config: {
+		redis: {
+			host: process.env.REDIS_HOST,
+			port: Number(process.env.REDIS_PORT),
+			password: process.env.REDIS_PASSWORD || undefined,
+		},
+	},
+	driver: 'redis',
+} as ICacheConfig;
