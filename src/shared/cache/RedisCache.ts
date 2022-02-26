3baa54e9a9e4554d001331c6da9e7d6b7d45fd4e
RicardoSantos-99 <ricardo2010carvalho@hotmail.com>
2022-02-26 08:42:46 -0300

    feat(Global): cache class

---
+++ src/shared/cache/RedisCache.ts
@@ -0,0 +1,18 @@
+import Redis, { Redis as RedisClient } from 'ioredis';
+import cacheConfig from '@config/cache';
+
+export default class RedisCache {
+	private client: RedisClient;
+
+	constructor() {
+		this.client = new Redis(cacheConfig.config.redis);
+	}
+
+	public async save(key: string, value: any): Promise<void> {
+		console.log(key, value);
+	}
+
+	//public async recover<T>(key: string): Promise<T | null> {}
+
+	public async invalidate(key: string): Promise<void> {}
+}
