23c42cb8acf5dc4688ab8a16f49663260e74bbec
RicardoSantos-99 <ricardo2010carvalho@hotmail.com>
2022-03-07 20:05:43 -0300

    refactor(Products): change redis cache class for verify connection

--- src/modules/products/services/DeleteProductService.ts
+++ src/modules/products/services/DeleteProductService.ts
@@ -1,4 +1,4 @@
-import RedisCache from '@shared/cache/RedisCache';
+import redisCache from '@shared/cache/RedisCache';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import ProductRepository from '../typeorm/repositories/ProductsRepository';
@@ -14,7 +14,6 @@ class DeleteProductService {
 		const product = await productsRepository.findOne(id);
 
 		if (!product) throw new AppError('Product not found.');
-		const redisCache = new RedisCache();
 
 		await redisCache.invalidate('api-vendas-PRODUCT_LIST');
 		await productsRepository.remove(product);
