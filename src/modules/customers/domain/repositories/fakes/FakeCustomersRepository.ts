7ffafba78f81bf7f64d3e1f467cb3a647c35144c
RicardoSantos-99 <ricardo2010carvalho@hotmail.com>
2022-03-16 22:29:42 -0300

    test(Customer): create first test for customer service

--- src/modules/customers/domain/repositories/fakes/FakeCustomersRepository.ts
+++ src/modules/customers/domain/repositories/fakes/FakeCustomersRepository.ts
@@ -3,9 +3,7 @@ import Customer from '@modules/customers/infra/typeorm/entities/Customer';
 import { ICustomersRepository } from '@modules/customers/domain/repositories/ICustomersRepository';
 import { ICreateCustomer } from '@modules/customers/domain/models/ICreateCustomer';
 
-class FakeCustomersRepository
-	implements Omit<ICustomersRepository, 'findAll' | 'remove'>
-{
+class FakeCustomersRepository implements ICustomersRepository {
 	private customers: Customer[] = [];
 
 	public async create({ name, email }: ICreateCustomer): Promise<Customer> {
@@ -23,6 +21,18 @@ class FakeCustomersRepository
 	public async save(customer: Customer): Promise<Customer> {
 		Object.assign(this.customers, customer);
 		return customer;
+	}
+
+	public async remove(customer: Customer): Promise<void> {
+		const customerIndex = this.customers.findIndex(
+			findCustomer => findCustomer.id === customer.id,
+		);
+
+		this.customers.splice(customerIndex, 1);
+	}
+
+	public async findAll(): Promise<Customer[]> {
+		return this.customers;
 	}
 
 	public async findByName(name: string): Promise<Customer | undefined> {
