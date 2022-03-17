07c8e2e3500a13b73d74e00ba74f4a35992065e7
RicardoSantos-99 <ricardo2010carvalho@hotmail.com>
2022-03-17 20:54:07 -0300

    feat(User): create fake hash provider and inject bcript provider in create session

---
+++ src/modules/users/providers/HashProvider/fakes/FakeHashProvider.ts
@@ -0,0 +1,16 @@
+import { IHashProvider } from '@modules/users/providers/HashProvider/models/IHashProvider';
+
+class BcryptHashProvider implements IHashProvider {
+	public async generateHash(payload: string): Promise<string> {
+		return payload;
+	}
+
+	public async compareHash(
+		payload: string,
+		hashed: string,
+	): Promise<boolean> {
+		return payload === hashed;
+	}
+}
+
+export default BcryptHashProvider;
